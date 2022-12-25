7a95ce9018a9ec855c4468737ddbe83060f90df5
dandy <dandy03@gmail.com>
2022-12-25 12:42:30 +0800

    viewCheckout function added, checkouts -> receipts

--- main.cpp
+++ main.cpp
@@ -48,10 +48,18 @@ struct Checkout {
     string date;
 };
 
+struct Receipt {
+    string email;
+    int product;
+    int quantity;
+    string date;
+};
+
 vector<Account> accounts;
 vector<Product> products;
 vector<Cart> carts;
 vector<Checkout> checkouts;
+vector<Receipt> receipts;
 
 bool loginStatus = false;
 string loginEmail = "";
@@ -80,8 +88,9 @@ void medicine();
 void viewItem(int p_num);
 void cart();
 void menuCart(int p_num);
+double checkoutList();
 void checkout();
-void deleteProduct();
+void dedactProduct();
 void invalidInput();
 bool checkAccount(string accountType);
 string checkInput(string choose);
@@ -1677,27 +1686,96 @@ void editItem(int p_num) {
 
 void viewCheckouts() {
     string choose;
+    double total = 0;
 
     displayMenu();
 
-    cout << "|\t\t\t\t\tLIST OF CHECKOUTS\t\t\t\t\t|" << endl;
-    cout << "+-----------------------------------------------------------------------------------------------+" << endl;
+    cout << "|\t\t\t\t\t\t\t\tLIST OF CHECKOUTS\t\t\t\t\t\t\t\t┃" << endl;
+    cout << "┣━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━┫" << endl;
+    cout << "┃\t  Date  \t┃\t\t  Email  \t\t┃\t\tProduct Name\t\t┃\t  Quantity      ┃     Price   \t┃" << endl;
+    
+    if (receipts.size() > 0) {
+        cout << "┣━━━━━━━━━━━━━━━━━━━━━━━╋━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╋━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╋━━━━━━━━━━━━━━━━━━━━━━━╋━━━━━━━━━━━━━━━┫" << endl;
+        cout << "┃\t\t\t┃\t\t\t\t\t┃\t\t\t\t\t┃\t\t\t┃\t\t┃" << endl;
 
-    // if (products.size() > 0) {
-    //     for (int i = 0; i < products.size(); i++) {
-    //         cout << "[" << i << "] " << products[i].name << endl;
-    //     }
+        // display receipts
+        for (int i = 0; i < receipts.size(); i++) {
+            // don't display date and email if previous data is same
+            if (i > 0 && receipts[i].email == receipts[i - 1].email && receipts[i].date == receipts[i - 1].date) {
+                cout << "┃\t\t\t┃\t\t\t\t\t";
+                
+            } else { // display date and email
+                cout << "┃  " << receipts[i].date << "  ";
 
-    // } else {
-    //     cout << "No checkouts yet!" << endl;
-    // }
+                cout << "┃\t" << receipts[i].email;
 
-    cout << "+-----------------------------------------------------------------------------------------------+" << endl;
+                if (receipts[i].email.length() <= 15) cout << "\t\t\t";
+                else if (receipts[i].email.length() <= 23) cout << "\t\t";
+                else cout << "\t";
+            }
 
-    cout << "\t\t\t\t Input action: ";
+            int p_num = receipts[i].product;
+
+            cout << "┃\t✱  " << products[p_num].name; // display product
+
+            if (products[p_num].name.length() <= 10) cout << "\t\t\t";
+            else if (products[p_num].name.length() <= 20) cout << "\t\t";
+            else cout << "\t";
+
+            cout << "┃\t     " << receipts[i].quantity << "   \t┃    ₱ " << products[p_num].price << "  \t┃" << endl;
+
+            total += (receipts[i].quantity * products[p_num].price);
+
+            string details = products[p_num].animal + "\'s " + products[p_num].category;
+
+            cout << "┃\t\t\t┃\t\t\t\t\t┃\t  " << details << "\t";// display other details of product
+
+            if (details.length() <= 12) cout << "\t\t";
+            else if (details.length() <= 18) cout << "\t";
+
+            cout << "┃\t\t\t┃\t\t┃" << endl;
+
+            // display total and divider if there is more than 1 email with different date
+            if ((receipts[i].email != receipts[i + 1].email || receipts[i].date != receipts[i + 1].date) && i != receipts.size() - 1) {
+                cout << "┃\t\t\t┃\t\t\t\t\t┃\t\t\t\t\t┣━━━━━━━━━━━━━━━━━━━━━━━╋━━━━━━━━━━━━━━━┫" << endl;
+                cout << "┃\t\t\t┃\t\t\t\t\t┃\t\t\t\t\t┃\t   Total     \t┃    ₱ " << total << "  \t┃" << endl;
+                cout << "┣━━━━━━━━━━━━━━━━━━━━━━━╋━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╋━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╋━━━━━━━━━━━━━━━━━━━━━━━╋━━━━━━━━━━━━━━━┫" << endl;;
+                cout << "┃\t\t\t┃\t\t\t\t\t┃\t\t\t\t\t┃\t\t\t┃\t\t┃" << endl;
+
+                total = 0;
+
+            } else if (receipts[i].email == receipts[i + 1].email && receipts[i].date == receipts[i + 1].date) { // spacing if products is checkout with same email and date
+                cout << "┃\t\t\t┃\t\t\t\t\t┃\t\t\t\t\t┃\t\t\t┃\t\t┃" << endl;
+
+            } else if (i == receipts.size() - 1) { // display total for last record
+                cout << "┃\t\t\t┃\t\t\t\t\t┃\t\t\t\t\t┣━━━━━━━━━━━━━━━━━━━━━━━╋━━━━━━━━━━━━━━━┫" << endl;
+                cout << "┃\t\t\t┃\t\t\t\t\t┃\t\t\t\t\t┃\t   Total     \t┃    ₱ " << total << "  \t┃" << endl;
+            }
+        }
+
+        cout << "┗━━━━━━━━━━━━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━━━━━━━━┻━━━━━━━━━━━━━━━┛" << endl;
+
+    } else {
+        cout << "┣━━━━━━━━━━━━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━━━━━━━━┻━━━━━━━━━━━━━━━┫" << endl;
+        cout << "┃\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t┃" << endl;
+        cout << "┃\t\t\t\t\t\t\t\tNo checkouts record yet!\t\t\t\t\t\t\t\t┃" << endl;
+        cout << "┃\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t┃" << endl;
+        cout << "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛" << endl;
+    }
+
+    cout << "\t\t☛ Input [action]: ";
     cin >> choose;
     
-    menuAdmin(choose);
+    if (checkInput(choose) == "alpha" && choose.length() == 1) {
+        transform(choose.begin(), choose.end(), choose.begin(), ::tolower);
+
+        menuAdmin(choose);
+        viewCheckouts();
+
+    } else {
+        invalidInput();
+        viewInventory();
+    }
 }
 
 void menuCustomer(string choose) {
@@ -2266,9 +2344,8 @@ void menuCart(int p_num) {
     cart();
 }
 
-void checkout() {
-    string choose;
-    int p_count = 0, count = 0, choose1;
+double checkoutList() {
+    int p_count = 0, count = 0;
     double total = 0, money;
 
     displayMenu();
@@ -2329,13 +2406,23 @@ void checkout() {
     cout << "┃\t\t\t" << checkouts[0].date << "\t\t\t\t\t\t\tTotal:\t\t┃\t     ₱ " << total << "  \t\t┃" << endl;
     cout << "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛" << endl << endl;
 
+    return total;
+}
+
+void checkout() {
+    string choose;
+    int choose1;
+    double total, money;
+
+    checkoutList();
+    
     cout << "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓" << endl;
     cout << "┃\t\t\t\t\t\t\t\t  MODE OF PAYMENT      \t\t\t\t\t\t\t\t┃" << endl;
     cout << "┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫" << endl;
-    cout << "┃\t\t\t\t[1] CASH\t\t\t\t┃\t\t\t\t[2] PETCOIN\t\t\t\t┃" << endl;
+    cout << "┃\t\t\t\t[1] Cash\t\t\t\t┃\t\t\t\t[2] Petcoin\t\t\t\t┃" << endl;
     cout << "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛" << endl;
 
-    cout << "\t\t☛ Input [action]: ";
+    cout << "\t\t\t\t\t\t\t\t☛ Input [action]: ";
     cin >> choose;
 
     // check if string is inputted in an int variable
@@ -2344,6 +2431,11 @@ void checkout() {
         checkout();
 
     } else if (checkInput(choose) == "number" && stoi(choose) >= 1 && stoi(choose) <= 2) {
+
+        total = checkoutList();
+
+        if (stoi(choose) == 1) cout << "\t\tMode of Payment: Cash" << endl << endl;
+        else cout << "\t\tMode of Payment: Petcoin" << endl << endl;
         
         cout << "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓" << endl;
         cout << "┃\t\t\t\t[1] Checkout\t\t\t\t┃\t\t\t\t[0] Cancel\t\t\t\t┃" << endl;
@@ -2381,7 +2473,7 @@ void checkout() {
                             cin >> money;
 
                             if (money >= total) {
-                                deleteProduct();
+                                dedactProduct();
                                 money -= total;
 
                                 if (money != 0) cout << "\t\tYour change is ₱ " << money << endl;
@@ -2399,7 +2491,7 @@ void checkout() {
                                     if (accounts[i].money >= total) {
                                         cout << "\t\tThanks for purchasing!";
 
-                                        deleteProduct();
+                                        dedactProduct();
                                         accounts[i].money -= total; // dedacting total price from petcoin
 
                                     } else cout << "\t\tYour Petcoin is not enough for the total price you checkout, cash in to use Petcoin.";
@@ -2432,8 +2524,8 @@ void checkout() {
     }
 }
 
-// delete products that has been checkout
-void deleteProduct() {
+// dedact products that has been checkout
+void dedactProduct() {
     for (const auto& checkout : checkouts) {
         for (int i = 0; i < carts.size(); i++) {
             if (carts[i].product == checkout.product && carts[i].email == loginEmail) {
@@ -2442,9 +2534,10 @@ void deleteProduct() {
                 break;
             }
         }
-
-        products[checkout.product].stock -= checkout.quantity;
+        receipts.push_back({loginEmail, checkout.product, checkout.quantity, checkout.date}); // insert in reciept vector
+        products[checkout.product].stock -= checkout.quantity; // dedacting stocks
     }
+
 }
 
 void invalidInput() {
